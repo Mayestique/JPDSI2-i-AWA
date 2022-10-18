046bcd7cc706ae346a24637b45361ba775c513e5
marta <marta@DESKTOP-E0BNACA>
2022-10-18 16:14:39 +0200

    Kalkulator kredytowy

---
+++ Kalkulator_kredytowy/src/main/java/com/jsfcourse/calc/KredytBB.java
@@ -0,0 +1,80 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class KredytBB {
+	private String kwota;
+	private String procent;
+	private String ile;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+
+	public String getKwota() {
+		return kwota;
+	}
+
+	public void setKwota(String kwota) {
+		this.kwota = kwota;
+	}
+
+	public String getProcent() {
+		return procent;
+	}
+
+	public void setProcent(String procent) {
+		this.procent = procent;
+	}
+
+	public String getIle() {
+		return ile;
+	}
+
+	public void setIle(String ile) {
+		this.ile = ile;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	public boolean doTheMath() {
+		try {
+			double kwota = Double.parseDouble(this.kwota);
+			double procent = Double.parseDouble(this.procent);
+			double ile = Double.parseDouble(this.ile);
+
+			result = kwota + procent + ile;
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+	// Go to "showresult" if ok
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+}
